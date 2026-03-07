c26b0882a5f75e6fbe524fcabddb8ba5178c8c77
Tống Thành Đạt <84076965+tongthanhdat009@users.noreply.github.com>
2026-03-07 18:20:54 +0700

    setup composer

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '127.0.0.1',
+        port: 8000,
+        strictPort: true,
+        hmr: {
+            host: '127.0.0.1',
+        },
+    },
 });
